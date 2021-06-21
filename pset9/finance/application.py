16e001bc3b706cbc51cbe3f670a558ceb15577ea
mia.bajic <mia.bajic@morosystems.cz>
2021-06-21 22:19:41 +0200

    added registration page

--- pset9/finance/application.py
+++ pset9/finance/application.py
@@ -120,7 +120,36 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # ensure passwords match
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords do not match", 403)
+
+        # ensure user isn't already registered
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) == 1:
+            return apology("user already exists", 403)
+
+        # add user to the DB
+        else:
+            password_hashed = generate_password_hash(request.form.get("password"))
+            rows = db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get("username"), password_hashed)
+
+        return redirect("/")
+
+    else:
+        return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
